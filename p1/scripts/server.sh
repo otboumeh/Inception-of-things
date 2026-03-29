1eeaf6d3f872916976b834c8ad6ea178c1623374
otmane <otmane@IoT.myguest.virtualbox.org>
2026-03-29 15:56:47 +0200

    Primera parte

---
+++ p1/scripts/server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export INSTALL_K3S_EXEC="--write-kubeconfig-mode 644 --bind-address=192.168.56.110 --node-ip=192.168.56.110"
+
+# Instalación de K3s Server
+curl -sfL https://get.k3s.io | sh -
+
+# Copiar el token a la carpeta compartida para que el Worker lo pueda leer
+sudo cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
---
+++ p1/scripts/worker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Esperar a que el archivo del token aparezca en la carpeta compartida
+while [ ! -f /vagrant/node-token ]; do
+  sleep 2
+done
+
+export K3S_URL="https://192.168.56.110:6443"
+export K3S_TOKEN=$(cat /vagrant/node-token)
+export INSTALL_K3S_EXEC="--node-ip=192.168.56.111"
+
+# Instalación de K3s Agent (Worker)
+curl -sfL https://get.k3s.io | K3S_URL=$K3S_URL K3S_TOKEN=$K3S_TOKEN sh -
