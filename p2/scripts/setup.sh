3b6162a6a5b2c097e4c12cca1f2aaa1572d90fc9
eandres <test@test.com>
2026-03-28 23:53:44 -0500

    update general

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-curl -sfL https://get.k3s.io | sh -
+curl -sfL https://get.k3s.io | sh -s - \
+	--disable metrics-server \
+	--disable local-storage \
+	--node-ip 192.168.56.110
 
 sleep 10
 
